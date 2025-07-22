26856a24aefe8230e05d856e9977c50c7a21d9fc
lalith nivas <lalithnivasy@gmail.com>
2025-07-22 20:59:55 +0530

    refactor: remove placeholder text from signup section in login page

--- src/views/pages/login/Login.js
+++ src/views/pages/login/Login.js
@@ -63,10 +63,6 @@ const Login = () => {
                 <CCardBody className="text-center">
                   <div>
                     <h2>Sign up</h2>
-                    <p>
-                      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                      tempor incididunt ut labore et dolore magna aliqua.
-                    </p>
                     <Link to="/register">
                       <CButton color="primary" className="mt-3" active tabIndex={-1}>
                         Register Now!
