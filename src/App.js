f1a766af8ddd866093a8a17777c17b7ec8c31035
lalith nivas <lalithnivasy@gmail.com>
2025-09-18 15:20:20 +0530

    feat: add Projects page component and remove Invoice route references

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ const Register = React.lazy(() => import('./views/pages/register/Register'))
 const Page404 = React.lazy(() => import('./views/pages/page404/Page404'))
 const Page500 = React.lazy(() => import('./views/pages/page500/Page500'))
 const Verification = React.lazy(() => import('./views/pages/verification'))
-const Invoice = React.lazy(() => import('./views/pages/Invoice'))
+// const Invoice = React.lazy(() => import('./views/pages/Invoice'))
 
 const AgentRegistration = React.lazy(() =>
   import('./views/pages/register/register_agent')
@@ -82,7 +82,7 @@ const App = () => {
           <Route exact path="/404" name="Page 404" element={<Page404 />} />
           <Route exact path="/500" name="Page 500" element={<Page500 />} />
           <Route exact path="/verification" name="Verification" element={<Verification />} />
-          <Route exact path="/invoice" name="Invoice" element={<Invoice />} />
+          {/* <Route exact path="/invoice" name="Invoice" element={<Invoice />} /> */}
 
           {/* Protected routes */}
           <Route
---
+++ src/views/pages/register/Projects.js
@@ -0,0 +1,13 @@
+import { React } from 'react'
+import { CContainer, CRow, CCol, CForm, CFormInput } from '@coreui/react'
+export default function Projects() {
+    return (
+        <CContainer>
+            <CRow className="justify-content-center">
+                <CCol md={9} lg={7} xl={6}>
+                    
+                </CCol>
+            </CRow>
+        </CContainer>
+    )
+}
