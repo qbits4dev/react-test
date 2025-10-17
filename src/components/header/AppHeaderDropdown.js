dd6a28031d9d8b9663d03bae623bd58510643278
lalith nivas <lalithnivasy@gmail.com>
2025-10-17 14:47:55 +0530

    uploaded blank profile image

--- src/components/header/AppHeaderDropdown.js
+++ src/components/header/AppHeaderDropdown.js
@@ -23,7 +23,7 @@ import {
 } from '@coreui/icons'
 import CIcon from '@coreui/icons-react'
 
-import avatar8 from './../../assets/images/avatars/8.jpg'
+import avatar8 from './../../assets/images/avatars/blankprofile.png'
 
 const AppHeaderDropdown = () => {
   const navigate = useNavigate();
