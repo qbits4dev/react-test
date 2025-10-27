95b60373332d7d5fdb6ef19d1d3487456e8355f3
veeravikas99 <veeravikas99@gmail.com>
2025-10-27 06:01:48 +0000

    verification and footer mobile view

--- src/components/AppFooter.js
+++ src/components/AppFooter.js
@@ -3,18 +3,17 @@ import { CFooter } from '@coreui/react'
 
 const AppFooter = () => {
   return (
-    <CFooter className="px-4">
-      <div>
+    <CFooter className="px-4 d-flex flex-column flex-md-row align-items-center justify-content-between">
+      <div className="text-center text-md-start">
         <a href="https://www.sriadityadevelopers.com/" target="_blank" rel="noopener noreferrer">
-          {/* CoreUI */}
           AdityaDevelopers
         </a>
         <span className="ms-1">&copy; 2025 QBITS4DEV.</span>
       </div>
-      <div className="ms-auto">
+      <div className="text-center text-md-end mt-2 mt-md-0">
         <span className="me-1">Powered by</span>
         <a href="https://qbits4dev.com/" target="_blank" rel="noopener noreferrer">
-          QBITS4DEVELOPERS PVT LTD; 
+          QBITS4DEVELOPERS PVT LTD
         </a>
       </div>
     </CFooter>
