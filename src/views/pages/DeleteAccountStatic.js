98a91323b5aca84b93561baa3f09102121d219b3
atulitha <pandu.revanth@gmail.com>
2026-03-20 18:11:00 +0530

    add trailing slash routes for Privacy Policy and Delete Account pages; update redirection logic

--- src/views/pages/DeleteAccountStatic.js
+++ src/views/pages/DeleteAccountStatic.js
@@ -2,14 +2,21 @@ import React, { useEffect } from 'react'
 
 const DeleteAccountStatic = () => {
   useEffect(() => {
-    // Redirect to the static delete-account.html page in the project root
-    window.location.href = '/delete-account.html'
+    // Redirect to the static delete-account.html page in the project public output root
+    const basePath = window.location.pathname.replace(/\/$/, '')
+    const target = `${window.location.origin}${basePath}/delete-account.html`
+    window.location.href = target
   }, [])
 
   return (
     <div className="text-center p-4">
       <h3>Redirecting to Delete Account page...</h3>
-      <p>If you are not redirected automatically, <a href="/delete-account.html">click here</a>.</p>
+      <p>
+        If you are not redirected automatically,{' '}
+        <a href={`${window.location.pathname.replace(/\/$/, '')}/delete-account.html`}>
+          click here
+        </a>
+      </p>
     </div>
   )
 }
--- src/views/pages/PrivacyStatic.js
+++ src/views/pages/PrivacyStatic.js
@@ -2,14 +2,21 @@ import React, { useEffect } from 'react'
 
 const PrivacyStatic = () => {
   useEffect(() => {
-    // Redirect to the static privacy.html page in the project root
-    window.location.href = '/privacy.html'
+    // Redirect to the static privacy.html page in the project public output root
+    const basePath = window.location.pathname.replace(/\/$/, '')
+    const target = `${window.location.origin}${basePath}/privacy.html`
+    window.location.href = target
   }, [])
 
   return (
     <div className="text-center p-4">
       <h3>Redirecting to Privacy Policy...</h3>
-      <p>If you are not redirected automatically, <a href="/privacy.html">click here</a>.</p>
+      <p>
+        If you are not redirected automatically,{' '}
+        <a href={`${window.location.pathname.replace(/\/$/, '')}/privacy.html`}>
+          click here
+        </a>
+      </p>
     </div>
   )
 }
